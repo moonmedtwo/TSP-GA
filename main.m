1aabc3b43c738b3d4ed166c53cbd29da8ef77baf
Qanh Vu <1410149@hcmut.edu.vn>
2017-09-15 22:15:14 +0700

    Qanh test

--- main.m
+++ main.m
@@ -1,3 +1,4 @@
+%tu trum qua
 clc;
 clear;
 distance = [ 0      0.730 0.640 0.840 0.800 0.430 0.380 1.010;
@@ -25,34 +26,38 @@ best_path = x(best_factor);
 
 crossover_mat = roulette_selection(p);
 for tmp = 1:1000
-%crossover
-for i = 1:(popsize/2)
-    parent(i*2-1,:) = pop(crossover_mat(i*2-1),:);
-    parent(i*2,:) = pop(crossover_mat(i*2),:);
-    offspring(i*2-1,:) = parent(i*2-1,:);
-    offspring(i*2,:) = parent(i*2,:);
-    cross_point = randi([2 6]);
-    for j = 1:cross_point
-        m = find(offspring(i*2-1,:)==parent(i*2,j),1);
-        if (m > j)
-            offspring(i*2-1,m) = offspring(i*2-1,j);
-        end
-        offspring(i*2-1,j) = parent(i*2,j);
-        
-        n = find(offspring(i*2,:)==parent(i*2-1,j),1);        
-        if (n > j)
-            offspring(i*2,n) = offspring(i*2,j);
-        end        
-        offspring(i*2,j) = parent(i*2-1,j);
+    if mod(tmp,50) == 0
+        disp('running')
     end
-end
-for i = 1:popsize
-    x(i) = total_dis(offspring(i,:));
-end
-[b,c] = min(x);
-if b < best_path
-    best_path = b;
-    offspring(1,:) = offspring(c,:);
-end
+        
+    %crossover
+    for i = 1:(popsize/2)
+        parent(i*2-1,:) = pop(crossover_mat(i*2-1),:);
+        parent(i*2,:) = pop(crossover_mat(i*2),:);
+        offspring(i*2-1,:) = parent(i*2-1,:);
+        offspring(i*2,:) = parent(i*2,:);
+        cross_point = randi([2 6]);
+        for j = 1:cross_point
+            m = find(offspring(i*2-1,:)==parent(i*2,j),1);
+            if (m > j)
+                offspring(i*2-1,m) = offspring(i*2-1,j);
+            end
+            offspring(i*2-1,j) = parent(i*2,j);
+
+            n = find(offspring(i*2,:)==parent(i*2-1,j),1);        
+            if (n > j)
+                offspring(i*2,n) = offspring(i*2,j);
+            end        
+            offspring(i*2,j) = parent(i*2-1,j);
+        end
+    end
+    for i = 1:popsize
+        x(i) = total_dis(offspring(i,:));
+    end
+    [b,c] = min(x);
+    if b < best_path
+        best_path = b;
+        offspring(1,:) = offspring(c,:);
+    end
 end
 best_path
